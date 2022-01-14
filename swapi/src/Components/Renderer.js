1175d64dab69d91991ae8352239f2aac452f5bdc
Erik <43432787+ErikLager@users.noreply.github.com>
2022-01-14 10:16:03 +0100

    Added 'Styling'

--- swapi/src/Components/Renderer.js
+++ swapi/src/Components/Renderer.js
@@ -4,25 +4,31 @@ const Renderer = (movies) => {
     let titles = [];
     let dates = [];
     let print = [];
-    
+
     // Adds all the titles to the titles array
     movies.data.forEach(element => {
         titles.push(element.title);
     });
-    
+
     // Adds all the dates of first release to an array
     movies.data.forEach(element => {
         dates.push(element.release_date);
     });
-    
+
     // Combines the two arrays into a single array for easier rendering
     for (let i = 0; i < titles.length; i++) {
-        print.push(titles[i] + " " + dates[i]);
+        print.push(titles[i] + " " + dates[i] + " ");
     }
-    
+
     return (
         <>
-            {print.map((element, index) => <Link to={`${index}`} key={index}>{element}</Link>)}
+            <ul>
+                {print.map((element, index) =>
+                    <li>
+                        <Link to={`${index}`} key={index}>{element}</Link>
+                    </li>
+                )}
+            </ul>
         </>
     );
 };
